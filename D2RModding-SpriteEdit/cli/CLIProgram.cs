ca25b462df4ee257165f81de454b747a65839eec
myocytebd <myocytebd999@outlook.com>
2024-03-11 07:15:55 +0800

    Convert to SDK project and add a CLI entry
    Linux ready by Mono.
    
    Caveat: This two csproj structure screws up vscode intellisense badly.
    One workaround is to rename one csproj.

---
+++ D2RModding-SpriteEdit/cli/CLIProgram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace D2RModding_SpriteEdit
+{
+    static class CLIProgram
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                
+                Console.WriteLine($"Usage: {Environment.GetCommandLineArgs()[0]} [FILES]...");
+                return;
+            }
+            CLI.RunBatch(args);
+        }
+    }
+}
